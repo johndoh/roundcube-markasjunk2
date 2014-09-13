b000faeac77a53c2d8ff1ce6bf7146ff77cb2a9e
PhilW <roundcube@tehinterweb.co.uk>
2014-09-13 10:39:13 +0100

    tidy up sql a bit

--- drivers/sa_blacklist.php
+++ drivers/sa_blacklist.php
@@ -10,6 +10,10 @@
 class markasjunk2_sa_blacklist
 {
 	private $sa_user;
+	private $sa_table;
+	private $sa_username_field;
+	private $sa_preference_field;
+	private $sa_value_field;
 
 	public function spam($uids, $mbox)
 	{
@@ -25,6 +29,10 @@ class markasjunk2_sa_blacklist
 	{
 		$rcmail = rcube::get_instance();
 		$this->sa_user = $rcmail->config->get('sauserprefs_userid', "%u");
+		$this->sa_table = $rcmail->config->get('sauserprefs_sql_table_name');
+		$this->sa_username_field = $rcmail->config->get('sauserprefs_sql_username_field');
+		$this->sa_preference_field = $rcmail->config->get('sauserprefs_sql_preference_field');
+		$this->sa_value_field = $rcmail->config->get('sauserprefs_sql_value_field');
 
 		$identity_arr = $rcmail->user->get_identity();
 		$identity = $identity_arr['email'];
@@ -59,21 +67,21 @@ class markasjunk2_sa_blacklist
 			if ($spam) {
 				// delete any whitelisting for this address
 				$db->query(
-					"DELETE FROM ". $rcmail->config->get('sauserprefs_sql_table_name') ." WHERE ". $rcmail->config->get('sauserprefs_sql_username_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_preference_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_value_field') ." = ?;",
+					"DELETE FROM {$this->sa_table} WHERE {$this->sa_username_field} = ? AND {$this->sa_preference_field} = ? AND {$this->sa_value_field} = ?;",
 					$this->sa_user,
 					'whitelist_from',
 					$email);
 
 				// check address is not already blacklisted
 				$sql_result = $db->query(
-								"SELECT value FROM ". $rcmail->config->get('sauserprefs_sql_table_name') ." WHERE ". $rcmail->config->get('sauserprefs_sql_username_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_preference_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_value_field') ." = ?;",
+								"SELECT value FROM {$this->sa_table} WHERE {$this->sa_username_field} = ? AND {$this->sa_preference_field} = ? AND {$this->sa_value_field} = ?;",
 								$this->sa_user,
 								'blacklist_from',
 								$email);
 
 				if (!$db->fetch_array($sql_result)) {
 					$db->query(
-						"INSERT INTO ". $rcmail->config->get('sauserprefs_sql_table_name') ." (". $rcmail->config->get('sauserprefs_sql_username_field') .", ". $rcmail->config->get('sauserprefs_sql_preference_field') .", ". $rcmail->config->get('sauserprefs_sql_value_field') .") VALUES (?, ?, ?);",
+						"INSERT INTO {$this->sa_table} ({$this->sa_username_field}, {$this->sa_preference_field}, {$this->sa_value_field}) VALUES (?, ?, ?);",
 						$this->sa_user,
 						'blacklist_from',
 						$email);
@@ -85,21 +93,21 @@ class markasjunk2_sa_blacklist
 			else {
 				// delete any blacklisting for this address
 				$db->query(
-					"DELETE FROM ". $rcmail->config->get('sauserprefs_sql_table_name') ." WHERE ". $rcmail->config->get('sauserprefs_sql_username_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_preference_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_value_field') ." = ?;",
+					"DELETE FROM {$this->sa_table} WHERE {$this->sa_username_field} = ? AND {$this->sa_preference_field} = ? AND {$this->sa_value_field} = ?;",
 					$this->sa_user,
 					'blacklist_from',
 					$email);
 
 				// check address is not already whitelisted
 				$sql_result = $db->query(
-								"SELECT value FROM ". $rcmail->config->get('sauserprefs_sql_table_name') ." WHERE ". $rcmail->config->get('sauserprefs_sql_username_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_preference_field') ." = ? AND ". $rcmail->config->get('sauserprefs_sql_value_field') ." = ?;",
+								"SELECT value FROM {$this->sa_table} WHERE {$this->sa_username_field} = ? AND {$this->sa_preference_field} = ? AND {$this->sa_value_field} = ?;",
 								$this->sa_user,
 								'whitelist_from',
 								$email);
 
 				if (!$db->fetch_array($sql_result)) {
 					$db->query(
-						"INSERT INTO ". $rcmail->config->get('sauserprefs_sql_table_name') ." (". $rcmail->config->get('sauserprefs_sql_username_field') .", ". $rcmail->config->get('sauserprefs_sql_preference_field') .", ". $rcmail->config->get('sauserprefs_sql_value_field') .") VALUES (?, ?, ?);",
+						"INSERT INTO {$this->sa_table} ({$this->sa_username_field}, {$this->sa_preference_field}, {$this->sa_value_field}) VALUES (?, ?, ?);",
 						$this->sa_user,
 						'whitelist_from',
 						$email);
