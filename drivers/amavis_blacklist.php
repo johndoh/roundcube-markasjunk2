5b412366afdcbe467705bfaabafc2d436c295f12
PhilW <roundcube@tehinterweb.co.uk>
2015-02-10 19:49:07 +0000

    add license headers

--- drivers/amavis_blacklist.php
+++ drivers/amavis_blacklist.php
@@ -5,6 +5,23 @@
  * @version 1.0
  * @requires Amacube plugin
  * @author Der-Jan
+ *
+ * Copyright (C) 2014 Der-Jan
+ *
+ * This driver is part of the MarkASJunk2 plugin for Roundcube.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Roundcube. If not, see http://www.gnu.org/licenses/.
  */
 
 /* #   'SELECT wb'.
--- drivers/cmd_learn.php
+++ drivers/cmd_learn.php
@@ -6,6 +6,23 @@
  * @author Philip Weir
  * Patched by Julien Vehent to support DSPAM
  * Enhanced support for DSPAM by Stevan Bajic <stevan@bajic.ch>
+ *
+ * Copyright (C) 2009-2014 Philip Weir
+ *
+ * This driver is part of the MarkASJunk2 plugin for Roundcube.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Roundcube. If not, see http://www.gnu.org/licenses/.
  */
 
 class markasjunk2_cmd_learn
